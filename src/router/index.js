4d0b9a5c424da3bd11cd20c1a922a4e314f2f8b0
Nebojsa Pesic <nebasjoa@gmail.com>
2025-11-06 12:14:25 +0100

    HomeView is not lazy loaded anymore

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: "Home", component: () => import('@/views/HomeView.vue') },
+    { path: '/', name: "Home", component: HomeView },
     // { path: '/registration', name: 'Registration', component: () => import('@/views/RegistrationView.vue') },
     { path: '/login', name: 'Login', component: () => import('@/views/LoginView.vue') },
   ],
